01d75babb91ca3d5e10d086838414f9945de320c
kishan <kspatel2018@gmail.com>
2016-02-15 20:34:46 -0500

    HW2
    Create blog models and display all blogs on home page

--- zhw1/src/frontpage/views.py
+++ zhw1/src/frontpage/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(request):
-	template = "home.html"
-	context = {
-	}
-	return render(request,template,context)
+    all_objects= blog.objects.all()
+    first_object= all_objects[0]
+    title_first_object= first_object.title 
+    template="home.html" 
+    context={
+        "all_objects": all_objects,
+        "first_object": first_object, 
+    }
+    return render(request,template,context)
