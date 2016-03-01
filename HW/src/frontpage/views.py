ec9eb416ced13e163d01a42aff5cccc4b6f50e55
kishan <kspatel2018@gmail.com>
2016-03-01 11:51:56 -0500

    add form to homepage

--- HW/src/frontpage/views.py
+++ HW/src/frontpage/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from .models import *
+
+from .forms import BlogPosts 
+
+
 # Create your views here.
 def home(request):
     all_objects= blog.objects.all()
     first_object= all_objects[0]
     title_first_object= first_object.title 
+
     template="home.html" 
+
+    form = BlogPosts(request.POST or None)
+
+    if form.is_valid():
+        variable = form.save(commit='false')
+        variable.save() 
+
     context={
+    	"form": form,
         "all_objects": all_objects,
         "first_object": first_object, 
     }
